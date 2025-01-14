b5afb96cac10e7e195fb87c70c2bb73863982932
Vishavs15 <vishvassbapodara@gmail.com>
2025-01-14 21:02:48 +0530

    Start Working on story  section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/Navbar'
+import Features from './components/Features'
+import Story from './components/Story'
+
 
 const App = () => {
   return (
     <>
       <div className="relative min-h-screen w-screen overflow-x-hidden">
+        <Navbar />
         <Hero />
         <About />
+        <Features />
+        <Story />
       </div>
     </>
   )
